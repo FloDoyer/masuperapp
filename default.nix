030f00e7d06735319dbf7bbb0e42d0f9f057408c
TODO <TODO>
2024-02-29 11:58:31 +0100

    default.nix

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 with import <nixpkgs> {};
 
 let
-  # masuperlib-src = fetchTarball "TODO";
-  masuperlib-src = ../masuperlib;
+  masuperlib-src = fetchTarball "https://github.com/FloDoyer/masuperlib/archive/refs/tags/v0.1.tar.gz";
+  #masuperlib-src = ../masuperlib;
 
   masuperlib = callPackage masuperlib-src {};
 
@@ -12,4 +12,3 @@ in stdenv.mkDerivation {
     nativeBuildInputs = [ cmake ];
     buildInputs = [ masuperlib ];
 }
-
